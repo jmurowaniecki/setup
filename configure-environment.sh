a6ae1a5c9838e6f8e7839fb51977698319a1cd65
John Murowaniecki <jmurowaniecki@gmail.com>
2019-04-01 10:56:39 -0300

    backup older ssh keys

--- configure-environment.sh
+++ configure-environment.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+for     each in ~/.ssh/id_rsa{,.pub}
+do cp "$each" "~/.ssh/${each}_$(date +'%Y%m%d-%H%M%S-%s')"
+done
+
 ssh-keygen -t rsa -P "" \
   -b 4096 -C "${EMAIL}" \
   -f    ~/.ssh/id_rsa
